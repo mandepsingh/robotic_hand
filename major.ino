1c1d1e4700e87520272c3ff8f351234295483c85
Mandeep singh <39272041+mandepsingh@users.noreply.github.com>
2019-12-19 22:24:53 +0530

    Add files via upload

---
+++ major.ino
@@ -0,0 +1,50 @@
+#include<Servo.h>
+Servo mandy1;
+Servo mandy2;
+
+Servo mandy3;
+
+Servo mandy4;
+
+Servo mandy5;
+
+void setup() {
+  pinMode(A0,INPUT);
+  
+  pinMode(A1,INPUT);
+  
+  pinMode(A2,INPUT);
+  
+  pinMode(A3,INPUT);
+  
+  pinMode(A4,INPUT);
+  
+  pinMode(A5,INPUT);
+
+  mandy1.attach(11);
+    
+  mandy2.attach(9);
+    
+  mandy3.attach(5);
+    
+  mandy4.attach(8);
+    
+  mandy5.attach(10);
+    int ma[5];
+    }
+void loop() {int pos=0;
+int flex1=analogRead(A0);
+
+int flex2=analogRead(A1);
+
+int flex3=analogRead(A2);
+
+int flex4=analogRead(A3);
+
+int flex5=analogRead(A4);
+ int pos1=map(flex1,400,700,0,180);
+ pos1=constrain(pos1,0,180);
+ 
+ mandy1.write(pos1); // put your main code here, to run repeatedly:
+
+}
---
+++ wired_hand.ino
@@ -0,0 +1,40 @@
+#include <Servo.h>
+Servo mandy;
+Servo mandy1;
+Servo mandy2;
+Servo mandy3;
+Servo mandy4;
+int val,val1,val2,val3,val4;
+
+void setup() {
+  // put your setup code here, to run once:
+mandy.attach(3);
+mandy1.attach(5);
+mandy2.attach(6);
+mandy3.attach(9);
+mandy4.attach(10);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+val=analogRead(A0);
+val=map(val, 0, 1023, 0, 180);
+mandy.write(val);
+
+val1=analogRead(A1);
+val1=map(val1, 0, 1023, 0, 180);
+mandy1.write(val1);
+
+val2=analogRead(A2);
+val2=map(val2, 0, 1023, 0, 180);
+mandy2.write(val2);
+
+val3=analogRead(A3);
+val3=map(val3, 0, 1023, 0, 180);
+mandy3.write(val3);
+
+val4=analogRead(A4);
+val4=map(val4, 0, 1023, 0, 180);
+mandy4.write(val4);
+delay(15);
+}
